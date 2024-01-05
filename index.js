805103b4c5420c5e798e8c488704550f4100e556
Jeni0701 <149695274+Jeni0701@users.noreply.github.com>
2024-01-05 22:21:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import {BrowserRouter,Routes,Route} from "react-router-dom"
-// import Home from "./components/home.js"
-// import Card from "./components/card.js"
 import Order from './components/order.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/* <Home/> */}
-    {/* <Card/> */}
-    <Order/>
-   
+   <Card/> 
   </React.StrictMode>
 )
 
